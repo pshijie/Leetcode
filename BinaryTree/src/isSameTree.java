9b7ac9bbb2f01f70d9ec5bb6e027de12a8f158bf
psj <729589165@qq.com>
2022-05-07 09:30:41 +0800

    Leetcode100-相同的树

---
+++ BinaryTree/src/isSameTree.java
@@ -0,0 +1,47 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author psj
+ * @date 2022/5/7 9:04
+ * @File: isSameTree.java
+ * @Software: IntelliJ IDEA
+ */
+// https://leetcode-cn.com/problems/same-tree/
+// 给你两棵二叉树的根节点p和q ，编写一个函数来检验这两棵树是否相同。如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的
+
+public class isSameTree {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+}
